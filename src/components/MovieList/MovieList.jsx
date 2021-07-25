6608807c0cf0ec7562bfad6c4b7fec0588b87e75
Kashia Moua <kserenitym@gmail.com>
2021-07-25 16:42:36 -0500

    added Links to /addmovie on App.js file

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,16 +1,24 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { useHistory } from 'react-router';
 import './MovieList.css'
 
 function MovieList() {
 
     const dispatch = useDispatch();
     const movies = useSelector(store => store.movies);
+    const history= useHistory();
 
     useEffect(() => {
         dispatch({ type: 'FETCH_MOVIES' });
     }, []);
 
+    const handleClick= (movie) => {
+        event.preventDefault();
+        dispatch({ type: 'GET_MOVIE_DETAILS', payload: movie})
+        history.push(`/details`);
+    }
+   
     return (
         <main>
             <h1>MovieList</h1>
@@ -19,7 +27,7 @@ function MovieList() {
                     return (
                         <div key={movie.id} >
                             <h3>{movie.title}</h3>
-                            <img src={movie.poster} alt={movie.title}/>
+                            <img src={movie.poster} alt={movie.title} onClick= {handleClick}/>
                         </div>
                     );
                 })}
